73412c099cff5a826cde8aa2ce2d2611d9ca8bb5
Eliot <eliot_van@hotmail.com>
2017-05-23 02:55:25 +0200

    Improvised FormatTime, now uses a friendlier format .e.g "1h 32m 16.00s"

--- Source/Public/TrialsPlayerState.h
+++ Source/Public/TrialsPlayerState.h
@@ -21,6 +21,10 @@ public:
     UPROPERTY(Replicated)
     float LastScoreObjectiveTimer;
 
+    /* The player's record time for the current active objective. <= 0 == N/A */
+    UPROPERTY(Replicated)
+    float ObjectiveRecordTime;
+
     void StartObjectiveTimer()
     {
         bIsObjectiveTimerActive = true;
@@ -43,6 +47,13 @@ public:
         return RoundTime((bIsObjectiveTimerActive ? GetWorld()->RealTimeSeconds : ObjectiveEndTime) - ObjectiveStartTime);
     }
 
+    float GetObjectiveRemainingTime()
+    {
+        return bIsObjectiveTimerActive 
+            ? (ActiveObjectiveInfo ? ActiveObjectiveInfo->RecordTime : 0.00) - GetObjectiveTimer()
+            : ObjectiveRecordTime;
+    }
+
     void SetObjective(ATrialsObjectiveInfo* objectiveInfo)
     {
         ActiveObjectiveInfo = objectiveInfo;
@@ -50,29 +61,36 @@ public:
 
     float RoundTime(float time)
     {
-        return roundf(time * 100.0) / 100.0;
+        return roundf(time*100.0)/100.0;
     }
 
     FText FormatTime(float value)
     {
         float seconds = fabs(value);
-        int32 minutes = (int32)seconds / 60;
-        int32 hours = minutes / 60;
-        seconds = seconds - (minutes * 60);
-        minutes = minutes - (hours * 60);
+        int32 minutes = (int32)seconds/60;
+        int32 hours = minutes/60;
+        seconds = seconds - (minutes*60.f);
+        minutes = minutes - (hours*60);
 
-        FString hourString, minuteString, secondString, output;
-        if (seconds < 10) secondString = TEXT("0") + FString::SanitizeFloat(seconds); else secondString = FString::SanitizeFloat(seconds);
-        if (minutes < 10) minuteString = TEXT("0") + minutes; else minuteString = FString::FromInt(minutes);
-        if (hours < 10) hourString = TEXT("0") + hours; else hourString = FString::FromInt(hours);
+        FString secondsString = FString::Printf(TEXT("%f"), seconds);
+        secondsString = secondsString.Left(secondsString.Find(".") + 3).Append("s");
+        if (minutes != 0 && seconds < 10.f) 
+            secondsString = TEXT("0") + secondsString;
 
+        FString minutesString = (hours != 0 && minutes < 10) 
+            ? TEXT("0") + minutes 
+            : FString::FromInt(minutes);
+
+        FString hoursString = FString::FromInt(hours);
+
+        FString output;
         if (hours != 0)
-            output = hourString + TEXT(":");
+            output = hoursString + TEXT("h ");
 
         if (minutes != 0)
-            output += minuteString + TEXT(":");
+            output += minutesString + TEXT("m ");
 
-        output = value < 0 ? TEXT("-") + output + secondString : output + secondString;
+        output = value < 0 ? TEXT("-") + output + secondsString : output + secondsString;
         return FText::FromString(output);
     }
 };
