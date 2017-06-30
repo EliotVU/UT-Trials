a0bab1ab4ca0b8913b8f1bccc8e371735175cb79
Eliot <eliot_van@hotmail.com>
2017-06-30 22:37:36 +0200

    WIP objective.

--- Source/Public/TrialsObjective.h
+++ Source/Public/TrialsObjective.h
@@ -12,11 +12,4 @@ class TRIALS_API ATrialsObjective : public AActor
 public:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, NoClear, Category = Objective)
     ATrialsObjectiveInfo* ObjectiveInfo;
-	
-    virtual void BeginPlay() override;
-    virtual void PostRenderFor(APlayerController* PC, UCanvas* Canvas, FVector CameraPosition, FVector CameraDir) override;
-    virtual FVector GetAdjustedScreenPosition(UCanvas* Canvas, const FVector& WorldPosition, const FVector& ViewPoint, const FVector& ViewDir, float Dist, float Edge, bool& bDrawEdgeArrow);
-
-private:
-    bool bBeaconWasLeft;
  };
